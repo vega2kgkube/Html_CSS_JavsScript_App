63d692f04e956b40f420be28bb3e5157927739aa
vega2kgkube <vega2kgkube@gmail.com>
2025-08-27 09:25:33 +0900

    Error msg 출력

--- 00student/js/form.js
+++ 00student/js/form.js
@@ -103,15 +103,15 @@ function loadStudents() {
     console.log("학생 목록 Load 중.....");
     fetch(`${API_BASE_URL}/api/students`) //Promise
         .then((response) => {
-            if (!response.ok) {
-                throw new Error("학생 목록을 불러오는데 실패했습니다!.");
-            }
+            // if (!response.ok) {
+            //     throw new Error("<<< 학생 목록을 불러오는데 실패했습니다!. ");
+            // }
             return response.json();
         })
         .then((students) => renderStudentTable(students))
         .catch((error) => {
-            console.log("Error: " + error);
-            alert("학생 목록을 불러오는데 실패했습니다!.");
+            console.log(error);
+            alert(">>> 학생 목록을 불러오는데 실패했습니다!.");
         });
 };
 
